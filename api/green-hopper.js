e9a0a013d4ab4ac2c9490c78101aaf561637ecbc
kcgolden <kcgolden@gmail.com>
2018-02-06 13:21:45 -0600

    getstat fn

--- api/green-hopper.js
+++ api/green-hopper.js
@@ -17,4 +17,19 @@ function GreenHopperBoardClient(jiraClient) {
         };
         return this.jiraClient.makeRequest(options, callback);
     };
+
+    this.getControlChart = function(opts, callback) {
+        var options = {
+            uri: this.jiraClient.buildGreenHopperURL('/rapid/charts/controlchart.json'),
+            method: 'GET',
+            json: true,
+            followAllRedirects: true,
+            qs: {
+                rapidViewId: opts.boardId,
+                swimlaneId: opts.swimlaneIdList,
+                _: new Date()
+            }
+        };
+        return this.jiraClient.makeRequest(options, callback);
+    };
 };
